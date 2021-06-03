b256bde27ad6dfd20b9708bc4daca032f17143f3
slibhin <>
2021-06-03 05:23:00 +0000

    Added question feedback

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,40 +5,42 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
+ // TODO 1.1b: Ask for candidate's name //
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+//console.log(question);
+candidateAnswer = input.question(question) ;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
   
-
-  return grade;
+  if (candidateAnswer === correctAnswer) {
+    console.log("Correct!");
+  } else {
+    console.log("Incorrect.")
+  }
+let grade;
+return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
